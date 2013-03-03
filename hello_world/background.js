4e18dcd832602a79b6f2b5dafe360c247dd27a8d
kojima <kunio@delorean.(none)>
2013-03-04 00:02:20 +0900

    first commit

---
+++ hello_world/background.js
@@ -0,0 +1,10 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // var action_url = "javascript:window.print();";
+  // chrome.tabs.update(tab.id, {url: action_url});
+
+		// chrome.browserAction.setTitle( {title:"aaa", tabId: tab.id} );
+		// chrome.tabs.create( {url:"http://www.google.com"} );
+		// chrome.tabs.update( tab.id, {url:"http://www.google.com"} );
+
+
+});
