b5acdde4e0d8b74620e775f82a7f72bdfff9b93f
Kunio Kojima <kunio@seiko.(none)>
2013-03-05 19:44:24 +0900

    succeed in  getting youtube list feed

--- hello_world/ylistrandom.js
+++ hello_world/ylistrandom.js
@@ -1,10 +1,12 @@
 function ylistrandom(youtube) {
+		// document.write("hoge");
 		var entries = youtube.feed.entry;
 		var url = [];
 		for (var i = 0; i < entries.length; ++i) {
-				url.push(entries[i].media$group.media$player[0].url.substr(31,11));
+				// url.push(entries[i].media$group.media$player[0].url.substr(31,11));
+				document.write(entries[i].title.$t);
+				document.write("</br>")
 		}
-		document.write("aaa");
 		var src= url[Math.floor(Math.random()*url.length)];
-		document.write('<embed src="http://www.youtube.com/v/' + src + '&hl=ja_JP&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="200" height="165"></embed>')
+		// document.write('<embed src="http://www.youtube.com/v/' + src + '&hl=ja_JP&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="200" height="165"></embed>')
 }
