ec58c914c7c23e53bfcef8dff8742155f8c1b6f9
Kunio Kojima <kunio@seiko.(none)>
2013-03-14 19:39:46 +0900

    succeed in random play of all videos

--- hello_world/ylistrandom.js
+++ hello_world/ylistrandom.js
@@ -1,22 +1,25 @@
 function ylistrandom(youtube) {
-		// document.write("hoge");
 		var entries = youtube.feed.entry;
-		var url = [];
-		var video_title = [];
+		list_titles = [];
+		video_titles = [];
+		video_urls = [];
 		var max_results = 50;
 		var turn_num;
-		for (var i = 0; i < entries.length; ++i) {
-				// for (var i = 0; i < 3; ++i) {
-				document.write(entries[i].title.$t);
-				document.write("</br>");
+		// for (var i = 0; i < entries.length; ++i) {
+		for (var i = 0; i < 3; ++i) {
+				// document.write(entries[i].title.$t);
+				// document.write("</br>");
+
 				// var list_url = entries[i].content.src + '&alt=json-in-script&callback=?';
 				// var list_url = entries[i].content.src + '&alt=json&callback=?';
 				var list_url = entries[i].content.src + '&alt=json';
-				document.write(list_url);
-				document.write("</br>");
+				list_titles.push(entries[i].title.$t);
 
-				video_titles = [];
-				video_urls = [];
+				// document.write(list_url);
+				// document.write("</br>");
+
+				// video_titles = [];
+				// video_urls = [];
 
 				// 50個ずつしか動画情報を取得できないので先に再生リスト内の全動画数からターン数を計算
 				$.ajax({
@@ -28,7 +31,7 @@ function ylistrandom(youtube) {
 						},
 						error: function(XMLHttpRequest,textStatus,errorThrown){ document.write(textStatus); }
 				});
-				document.write(String(turn_num)); document.write("</br>");
+				// document.write(String(turn_num)); document.write("</br>");
 
 				// 全動画情報取得
 				for( var turn = 0; turn < turn_num; ++turn ){
@@ -44,12 +47,16 @@ function ylistrandom(youtube) {
 										// document.write("</br>");
 										for ( var j = 0; j < video_entries.length; ++j ){
 												// document.write(video_entries[j].title.$t);
-												// document.write(video_entries[j].content.src);
+
+												try{
+														// document.write(video_entries[j].content.src);
+														video_urls.push(video_entries[j].content.src);
+														video_titles.push(video_entries[j].title.$t);
+												}catch(e){
+														// document.write("hoge");// 要素がなかった場合の例外処理
+												}
 												// document.write("</br>");
 
-												// 要素がなかった場合の例外処理が必要
-												video_titles.push(video_entries[j].title.$t);
-												video_urls.push(video_entries[j].content.src);
 										}
 								},
 								error: function(XMLHttpRequest,textStatus,errorThrown){
@@ -58,18 +65,18 @@ function ylistrandom(youtube) {
 						});
 				}
 
-				$.each( video_urls, function(i, val){
-						document.write(val);
-						document.write("</br>");
-				});
-
-
-				document.write("</br>");
+				// document.write("</br>");
 		}
 
-		// var src= url[Math.floor(Math.random()*url.length)];
-		document.write(video_urls[Math.floor(Math.random()*video_urls.length)]);
-		// document.write('<embed src="http://www.youtube.com/v/' + src + '&hl=ja_JP&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="200" height="165"></embed>')
+		// $.each( video_titles, function(i, val){
+		// 		document.write(val);
+		// 		document.write("</br>");
+		// });
+
+		// video_url = video_urls[Math.floor(Math.random()*video_urls.length)];
+		// video_title = video_titles[Math.floor(Math.random()*video_titles.length)];
+
+
 }
 
 // $(document).ready(function() {
@@ -88,3 +95,5 @@ function ylistrandom(youtube) {
 // });
 // $.ajaxSetup({async: true});
 // document.write(video_title[0]);
+
+// document.write('<embed src="http://www.youtube.com/v/' + src + '&hl=ja_JP&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="200" height="165"></embed>')
